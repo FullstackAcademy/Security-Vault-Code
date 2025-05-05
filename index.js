ca21509dd828f4f02af5f755720ffc7616e1556b
Sean Tucker <Tuckersean50@gmail.com>
2025-05-04 20:35:54 -0400

    Vault

---
+++ index.js
@@ -0,0 +1,8 @@
+let code01;
+code01 = 7 + 3;
+let code02 = 120 / 2 - 20;
+let code03 = 19 * 2 + (9 % 2);
+let message = "The vault has been secured. The combination is:";
+let codeA = code01 + "-" + code02 + "-" + code03;
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(message + codeA + code03);
